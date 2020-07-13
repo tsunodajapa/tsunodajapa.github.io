b05537db8c4531881f8d3e7041a80814abbf5322
tsunoda1 <tsunoda@b2ml.com.br>
2020-07-13 20:09:08 -0300

    Updates

---
+++ precache-manifest.7ad0b44db7278d3fc1ae6489b9037a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420b54da7aa62ecdf9a2be8639e1bbea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43d76d16595fbef2f47f",
+    "url": "/static/css/main.9de48a73.chunk.css"
+  },
+  {
+    "revision": "6d40977d2c02ea8d2fca",
+    "url": "/static/js/2.2d40bff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d40bff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d76d16595fbef2f47f",
+    "url": "/static/js/main.42f9727e.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  }
+]);
